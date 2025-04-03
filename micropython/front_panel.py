425354a8a14184cca8c897a766921fd9c1e5b113
Kevin Price <Keyvin@gmail.com>
2025-04-02 22:54:48 -0400

    fixed front panel bug

--- micropython/front_panel.py
+++ micropython/front_panel.py
@@ -76,8 +76,8 @@ def stop_emulator():
 
 
 #reverse for C.A. vs C.C.
-def out_digit(digit1,digit2, segment1, segment2):
-    for i in range(0,15):
+def out_digit(digit2,digit1, segment2, segment1):
+    for i in range(0,16):
         segment_pins[i].on()    
         
     for i in digits[digit1]:        
